5af899b8ddcc4d8b3b83eea76f4c1142fb2303b2
unnarb <unnarb@gmail.com>
2020-10-28 15:33:11 +0000

    Models

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ClientCard from './components/ClientCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+
+  const dummy = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const dummy2 = () => {
+
+  }
+
+  return( 
+    <div className='App'>
+      <h1>Þjónustusíður</h1>
+      <div className="clientcard__wrapper">
+        <ClientCard />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/ClientCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ClientCard = () => (<div>Client Card</div>)
+
+export default ClientCard

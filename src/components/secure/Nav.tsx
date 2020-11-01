c8197389bf7dddcede3366b2934c7d575b3d7dc5
unnarb <unnarb@gmail.com>
2020-11-01 15:32:07 +0000

    Links

--- src/components/secure/Nav.tsx
+++ src/components/secure/Nav.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
     <div className="nav__container">
         <nav className="nav__navigation">
             <ul>
-                <li>Vefir og smáforrit</li>
-                <li>Þjónustur</li>
-                <li>Notendur</li>
+                <li><Link to={'/clients'} className="nav__link" >Vefir og smáforrit</Link></li>
+                <li><Link to={'/resources'} className="nav__link" >Þjónustur</Link></li>
+                <li><Link to={'/users'} className="nav__link" >Notendur</Link></li>
             </ul>
         </nav>
     </div>
